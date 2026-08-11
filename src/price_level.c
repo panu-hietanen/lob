01797cf02db622ddd4177304f3fe52cf5486ee31
panu-hietanen <78078779+panu-hietanen@users.noreply.github.com>
2026-08-11 19:58:29 +0100

    Complete functions for adding and removing from the price level

--- src/price_level.c
+++ src/price_level.c
@@ -2,15 +2,53 @@
 
 void insert_back(PriceLevel* level, Order* order)
 {
-    return;
+    if (level->back)
+    {
+        level->back->next = order;
+    }
+    if (!level->front)
+    {
+        level->front = order;
+    }
+    order->prev = level->back;
+    order->next = NULL;
+    order->level = level;
+    
+    level->back = order;
+    level->length++;
+
 }
 
 void unlink_order(Order* order)
 {
-    return;
+    Order* prev = order->prev;
+    Order* next = order->next;
+    if (prev)
+    {
+        prev->next = next;
+    }
+    else
+    {
+        order->level->front = next;
+    }
+
+    if (next)
+    {
+        next->prev = prev;
+    }
+    else
+    {
+        order->level->back = prev;
+    }
+    
+    order->level->length--;
+
+    order->level = NULL;
+    order->next = NULL;
+    order->prev = NULL;
 }
 
 bool is_empty(const PriceLevel* level)
 {
-    return false;
+    return level->length == 0;
 }
