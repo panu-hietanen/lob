d0b2f116ff45c176ceb5b4975be6de375df44ab3
panu-hietanen <78078779+panu-hietanen@users.noreply.github.com>
2026-08-22 20:04:36 +0100

    Update header spacing for clarity

--- tests/test_tree.h
+++ tests/test_tree.h
@@ -6,12 +6,14 @@
 
 bool tree_insert_empty();
 bool tree_insert_level();
+
 bool tree_remove_root();
 bool tree_remove_no_children();
 bool tree_remove_one_child_right();
 bool tree_remove_one_child_left();
 bool tree_remove_two_children_one_hop();
 bool tree_remove_two_children_over_one_hop();
+
 bool tree_find_min();
 bool tree_find_max();
 
