802f310e3d7bed6bccb90186a98218f8dd8eac9b
panu-hietanen <78078779+panu-hietanen@users.noreply.github.com>
2026-08-22 15:27:23 +0100

    Add tests for cancelation and deletion

--- tests/test_book.c
+++ tests/test_book.c
@@ -132,6 +132,230 @@ bool book_add_to_level(Side side)
     return true;
 }
 
+bool book_delete(Side side)
+{
+    OrderBook* book = book_init();
+
+    OrderId id1 = 1;
+    Price price1 = 5;
+    u64 quantity1 = 100;
+    book_add_order(book, id1, price1, quantity1, side);
+
+    OrderId id2 = 2;
+    u64 quantity2 = 100;
+    book_add_order(book, id2, price1, quantity2, side);
+    
+    if (!book_delete_order(book, id2, side)) return false;
+    if (map_lookup(&book->ordersMap, id1) == NULL) return false;
+    if (map_lookup(&book->ordersMap, id2) != NULL) return false;
+
+    Map* map = (side == BUY) ? &book->bidMap : &book->askMap;
+    PriceLevel* level = map_lookup(map, price1);
+    if (level == NULL) return false;
+    if (level->length != 1) return false;
+
+    book_destroy(book);
+    return true;
+}
+
+bool book_delete_invalid(Side side)
+{
+    OrderBook* book = book_init();
+
+    OrderId id1 = 1;
+    Price price1 = 5;
+    u64 quantity1 = 100;
+    book_add_order(book, id1, price1, quantity1, side);
+
+    OrderId id2 = 2;
+    u64 quantity2 = 100;
+    book_add_order(book, id2, price1, quantity2, side);
+    
+    OrderId id3 = 3;
+    if (book_delete_order(book, id3, side)) return false;
+    if (map_lookup(&book->ordersMap, id1) == NULL) return false;
+    if (map_lookup(&book->ordersMap, id2) == NULL) return false;
+
+    Map* map = (side == BUY) ? &book->bidMap : &book->askMap;
+    PriceLevel* level = map_lookup(map, price1);
+    if (level == NULL) return false;
+    if (level->length != 2) return false;
+
+    book_destroy(book);
+    return true;
+}
+
+bool book_delete_update_best(Side side)
+{
+    OrderBook* book = book_init();
+
+    OrderId id1 = 1;
+    Price price1 = 5;
+    u64 quantity1 = 100;
+    book_add_order(book, id1, price1, quantity1, side);
+
+    OrderId id2 = 2;
+    Price price2 = (side == BUY) ? 6 : 4;
+    u64 quantity2 = 100;
+    book_add_order(book, id2, price2, quantity2, side);
+    PriceLevel* best = (side == BUY) ? book->bestBid : book->bestAsk;
+    if (best->price != price2) return false;
+    
+    if (!book_delete_order(book, id2, side)) return false;
+    if (map_lookup(&book->ordersMap, id1) == NULL) return false;
+    if (map_lookup(&book->ordersMap, id2) != NULL) return false;
+
+    PriceLevel* newBest = (side == BUY) ? book->bestBid : book->bestAsk;
+    if (newBest->price != price1) return false;
+
+    Map* map = (side == BUY) ? &book->bidMap : &book->askMap;
+    PriceLevel* level = map_lookup(map, price2);
+    if (level != NULL) return false;
+
+    book_destroy(book);
+    return true;   
+}
+
+bool book_delete_dont_update_best(Side side)
+{
+    OrderBook* book = book_init();
+
+    OrderId id1 = 1;
+    Price price1 = 5;
+    u64 quantity1 = 100;
+    book_add_order(book, id1, price1, quantity1, side);
+
+    OrderId id2 = 2;
+    Price price2 = (side == BUY) ? 6 : 4;
+    u64 quantity2 = 100;
+    book_add_order(book, id2, price2, quantity2, side);
+    PriceLevel* best = (side == BUY) ? book->bestBid : book->bestAsk;
+    if (best->price != price2) return false;
+    
+    if (!book_delete_order(book, id1, side)) return false;
+    if (map_lookup(&book->ordersMap, id1) != NULL) return false;
+    if (map_lookup(&book->ordersMap, id2) == NULL) return false;
+
+    PriceLevel* newBest = (side == BUY) ? book->bestBid : book->bestAsk;
+    if (newBest->price != price2) return false;
+
+    Map* map = (side == BUY) ? &book->bidMap : &book->askMap;
+    PriceLevel* level = map_lookup(map, price1);
+    if (level != NULL) return false;
+
+    book_destroy(book);
+    return true;   
+}
+
+bool book_cancel(Side side)
+{
+    OrderBook* book = book_init();
+
+    OrderId id1 = 1;
+    Price price1 = 5;
+    u64 quantity1 = 100;
+    book_add_order(book, id1, price1, quantity1, side);
+
+    OrderId id2 = 2;
+    u64 quantity2 = 100;
+    book_add_order(book, id2, price1, quantity2, side);
+    
+    u64 cancel_amount = 90;
+    if (!book_cancel_order(book, id2, cancel_amount, side)) return false;
+    if (map_lookup(&book->ordersMap, id1) == NULL) return false;
+    Order* newOrder = map_lookup(&book->ordersMap, id2);
+    if (newOrder->quantity != quantity2 - cancel_amount) return false;
+
+    Map* map = (side == BUY) ? &book->bidMap : &book->askMap;
+    PriceLevel* level = map_lookup(map, price1);
+    if (level == NULL) return false;
+    if (level->length != 2) return false;
+
+    book_destroy(book);
+    return true;
+}
+
+bool book_cancel_invalid(Side side)
+{
+    OrderBook* book = book_init();
+
+    OrderId id1 = 1;
+    Price price1 = 5;
+    u64 quantity1 = 100;
+    book_add_order(book, id1, price1, quantity1, side);
+
+    OrderId id2 = 2;
+    u64 quantity2 = 100;
+    book_add_order(book, id2, price1, quantity2, side);
+    
+    OrderId id3 = 3;
+    u64 cancel_amount = 90;
+    if (book_cancel_order(book, id3, cancel_amount, side)) return false;
+    if (map_lookup(&book->ordersMap, id1) == NULL) return false;
+    if (map_lookup(&book->ordersMap, id2) == NULL) return false;
+
+    Map* map = (side == BUY) ? &book->bidMap : &book->askMap;
+    PriceLevel* level = map_lookup(map, price1);
+    if (level == NULL) return false;
+    if (level->length != 2) return false;
+
+    book_destroy(book);
+    return true;
+}
+
+bool book_cancel_full_amount(Side side)
+{
+    OrderBook* book = book_init();
+
+    OrderId id1 = 1;
+    Price price1 = 5;
+    u64 quantity1 = 100;
+    book_add_order(book, id1, price1, quantity1, side);
+
+    OrderId id2 = 2;
+    u64 quantity2 = 100;
+    book_add_order(book, id2, price1, quantity2, side);
+    
+    u64 cancel_amount = 100;
+    if (!book_cancel_order(book, id2, cancel_amount, side)) return false;
+    if (map_lookup(&book->ordersMap, id1) == NULL) return false;
+    if (map_lookup(&book->ordersMap, id2) != NULL) return false;
+
+    Map* map = (side == BUY) ? &book->bidMap : &book->askMap;
+    PriceLevel* level = map_lookup(map, price1);
+    if (level == NULL) return false;
+    if (level->length != 1) return false;
+
+    book_destroy(book);
+    return true;
+}
+bool book_cancel_too_much(Side side)
+{
+    OrderBook* book = book_init();
+
+    OrderId id1 = 1;
+    Price price1 = 5;
+    u64 quantity1 = 100;
+    book_add_order(book, id1, price1, quantity1, side);
+
+    OrderId id2 = 2;
+    u64 quantity2 = 100;
+    book_add_order(book, id2, price1, quantity2, side);
+    
+    u64 cancel_amount = 110;
+    if (book_cancel_order(book, id2, cancel_amount, side)) return false;
+    if (map_lookup(&book->ordersMap, id1) == NULL) return false;
+    if (map_lookup(&book->ordersMap, id2) == NULL) return false;
+
+    Map* map = (side == BUY) ? &book->bidMap : &book->askMap;
+    PriceLevel* level = map_lookup(map, price1);
+    if (level == NULL) return false;
+    if (level->length != 2) return false;
+
+    book_destroy(book);
+    return true;
+}
+
 int main()
 {
     typedef struct TestCase {
@@ -144,6 +368,14 @@ int main()
         {"book_new_best_price", book_new_best_price},
         {"book_new_level", book_new_level},
         {"book_add_to_level", book_add_to_level},
+        {"book_delete", book_delete},
+        {"book_delete_invalid", book_delete_invalid},
+        {"book_delete_update_best", book_delete_update_best},
+        {"book_delete_dont_update_best", book_delete_dont_update_best},
+        {"book_cancel", book_cancel},
+        {"book_cancel_invalid", book_cancel_invalid},
+        {"book_cancel_full_amount", book_cancel_full_amount},
+        {"book_cancel_too_much", book_cancel_too_much},
     };
 
     int total = (int)(sizeof(tests) / sizeof(tests[0]));
--- tests/test_book.h
+++ tests/test_book.h
@@ -12,5 +12,13 @@ bool book_first_level(Side side);
 bool book_new_best_price(Side side);
 bool book_new_level(Side side);
 bool book_add_to_level(Side side);
+bool book_delete(Side side);
+bool book_delete_invalid(Side side);
+bool book_delete_update_best(Side side);
+bool book_delete_dont_update_best(Side side);
+bool book_cancel(Side side);
+bool book_cancel_invalid(Side side);
+bool book_cancel_full_amount(Side side);
+bool book_cancel_too_much(Side side);
 
 #endif // TEST_BOOK_H_
